f3a12269faf3eb37a1b59dd5bc778792c2250f06
Roger <roger-kueng@bluewin.ch>
2022-12-08 12:23:51 +0100

    Day06 solved

---
+++ 2022/Day06/solutionPart1.js
@@ -0,0 +1,30 @@
+const { arrayBuffer } = require("stream/consumers");
+const { readInputFile } = require("../helper");
+
+const inputList = readInputFile(__dirname).split("\n");
+
+getStartMarker(4);
+getStartMarker(14);
+
+function getStartMarker(distinctChars) {
+  for (let signal of inputList) {
+    let startPoint = 0;
+    while (true) {
+      let marker = signal.substr(startPoint, distinctChars);
+
+      let hasDuplicates = checkDuplicates(marker);
+
+      if (!hasDuplicates) {
+        console.log("marker after: " + (startPoint + distinctChars));
+        break;
+      }
+
+      startPoint++;
+      continue;
+    }
+  }
+}
+
+function checkDuplicates(marker) {
+  return new Set(marker).size !== marker.length;
+}
